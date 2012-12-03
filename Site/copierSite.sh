233e46da7a48fa20c49f678fa57d7f14c45747c0
LeSanglier <lesanglierdesardennes@gmail.com>
2012-12-03 20:12:59 +0100

    Modif : Création d'un lien symbolique

--- Site/copierSite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -r stendhal_website ../../PROD/Site
-cp -r stendhal_website ../../PROD/Lamp/lampp/htdocs/
-mv ../../PROD/Lamp/lampp/htdocs/stendhal_website ../../PROD/Lamp/lampp/htdocs/stendhal
---
+++ copierSite.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ln -s ./Site/stendhal_website ../../PROD/Lamp/lampp/htdocs/stendhal
