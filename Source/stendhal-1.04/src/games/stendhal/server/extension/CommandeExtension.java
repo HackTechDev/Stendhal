d813912cc489b55325ef9fab17e2185ca71b4b8f
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2013-02-15 23:21:09 +0100

    Ajout : Extension commannde

--- Source/stendhal-1.04/src/games/stendhal/server/extension/CommandeExtension.java
+++ Source/stendhal-1.04/src/games/stendhal/server/extension/CommandeExtension.java
@@ -1,5 +1,10 @@
 package games.stendhal.server.extension;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import games.stendhal.server.actions.ActionListener;
 import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.entity.player.Player;
@@ -12,8 +17,7 @@ import org.apache.log4j.Logger;
  *
  */
 
-public class CommandeExtension extends StendhalServerExtension implements
-		ActionListener {
+public class CommandeExtension extends StendhalServerExtension implements ActionListener {
 
 	private static final Logger logger = Logger.getLogger(CommandeExtension.class);
 
@@ -21,7 +25,7 @@ public class CommandeExtension extends StendhalServerExtension implements
 		super();
 		logger.info("CommandeExtension en-cours de démarrage...");
 		CommandCenter.register("commande", this);
-
+		System.out.println("CommandeExtension en-cours de démarrage...");
 	}
 
 	@Override
@@ -37,7 +41,7 @@ public class CommandeExtension extends StendhalServerExtension implements
 	}
 
 	private void onCommande(final Player player, final RPAction action) {
-		final String usage = "Usage : #/commande <nom du commande>";
+		final String usage = "Usage : #/commande <nom de la commande>";
 		String text = "";
 
 		boolean canActiverCommande = false;
@@ -50,10 +54,11 @@ public class CommandeExtension extends StendhalServerExtension implements
 				activerCommande = null;
 			}
 			if (activerCommande == null) {
-				player.sendPrivateText("Vous n'avez dit de commande à activer.");
-				logger.error("Le joueur n'a pas dit de commande.");
+				player.sendPrivateText("Vous n'avez pas dit de #commande à activer.");
+				logger.error("Le joueur n'a pas dit de #commande.");
 			} else {
-				player.sendPrivateText("Activation du commande en cours...");
+				player.sendPrivateText("Activation de la #commande en cours...");
+				System.out.println("Activation de #commande en cours...");
 			}
 		} else {
 			text = usage;
@@ -75,11 +80,51 @@ public class CommandeExtension extends StendhalServerExtension implements
 	}
 
 	private void activerCommande(final Player player, String commande) {
-		if (commande.contains("Information")) {
-			player.sendPrivateText("Information");
+		// Liste commande
+		Map<String, String> listeCommande = new HashMap<String, String>();
+		
+		listeCommande.put("information", "Information sur #commande");
+		listeCommande.put("liste", "Liste des #commande disponibles");
+		listeCommande.put("aide", "Aide sur #commande");
+		listeCommande.put("executer", "Executer une #commande");
+		listeCommande.put("cacher", "Cacher une #commande");
+		listeCommande.put("date", "Affiche la date");
+		listeCommande.put("heure", "Affiche l'heure");
+		listeCommande.put("dh", "Affiche la date et l'heure");
+
+		// Date et heure
+		Date dateMaintenant = new Date();
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat formatHeure = new SimpleDateFormat("hh:mm:ss");
+
+		System.out.println("************************************");
+
+		// Commandes
+		if (commande.contains("information")) {
+			player.sendPrivateText("information");
+			System.out.println("Argument : information");
+		} else if (commande.contains("liste")){
+			player.sendPrivateText("liste");
+			System.out.println("Argument : liste");
+			// Liste les commandes
+			for (Map.Entry<String,String> commandeLigne : listeCommande.entrySet()){
+			    System.out.println(commandeLigne.getKey() + " = " + commandeLigne.getValue());
+			}
+		} else if (commande.contains("date")){
+			player.sendPrivateText("date : " + formatDate.format(dateMaintenant));
+			System.out.println("Argument : date : " + formatDate.format(dateMaintenant));		
+		} else if (commande.contains("heure")){
+			player.sendPrivateText("heure : " + formatHeure.format(dateMaintenant));
+			System.out.println("Argument : heure : " + formatHeure.format(dateMaintenant));
+		} else if (commande.contains("dh")){
+			player.sendPrivateText(formatDate.format(dateMaintenant) + " a " + formatHeure.format(dateMaintenant));
+			System.out.println(formatDate.format(dateMaintenant) + " a " + formatHeure.format(dateMaintenant));
 		} else {
-			player.sendPrivateText("Le commande que vous essayer d'activer n'existe pas.");
+			player.sendPrivateText("La #commande que vous essayer d'activer n'existe pas.");
+			System.out.println("La #commande que vous essayer d'activer n'existe pas.");
 		}
+		
+		System.out.println("************************************");		
 	}
 
 }
